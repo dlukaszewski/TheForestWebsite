2bc0735ce06f92ebb158c7c9db14cdb0117738f4
dlukaszewski <lukaszewsidamian@gmail.com>
2022-11-04 00:14:51 +0100

    fixing handleScroll in JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const hamburgerLogo = document.querySelector(".hamburger-logo");
 const navMobile = document.querySelector(".nav-mobile");
 const navLinkMobile = document.querySelectorAll(".nav__link-mobile");
 const hamburgerBox = document.querySelector(".hamburger-box");
+const allSections = document.querySelectorAll([".black-section", ".white-section"]);
 
 const handleNav = () => {
 	hamburgerBtn.classList.toggle("is-active");
@@ -10,9 +11,8 @@ const handleNav = () => {
 	hamburgerBox.classList.toggle("hamburger-box--active");
 	hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon.png");
 	deleteAnimation();
-    turnWhiteLogo();
+	turnWhiteLogo();
 };
-hamburgerBtn.addEventListener("click", handleNav);
 
 const deleteAnimation = () => {
 	navLinkMobile.forEach(item => {
@@ -29,3 +29,37 @@ const turnWhiteLogo = () => {
 		hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon_white.png");
 	}
 };
+
+const handleScroll = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach(section => {
+		if (section.classList.contains("white-section") && section.offsetTop <= currentSection + 30) {
+			hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon.png");
+			hamburgerBox.style.border = "1px solid black";
+		} else if (section.classList.contains("black-section") && section.offsetTop <= currentSection + 30){
+			hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon_white.png");
+			hamburgerBox.style.border = "1px solid #fff";
+		}
+	});
+};
+// const handleScroll = () => {
+// 	const currentSection = window.scrollY;
+
+// 	allWhiteSections.forEach(section => {
+// 		if (
+// 			section.classList.contains("white-section") &&
+// 			section.offsetTop <= currentSection + 30
+// 		) {
+// 			hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon.png");
+// 			hamburgerBox.style.border = "1px solid black";
+// 		} else {
+// 			// turnWhiteLogo();
+// 			hamburgerLogo.setAttribute("src", "dist/img/tree_nature_icon_white.png");
+// 			hamburgerBox.style.border = "1px solid #fff";
+// 		}
+// 	});
+// };
+// handleScroll();
+hamburgerBtn.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleScroll);
